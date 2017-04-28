5aaa12f793f9bc3f530f7a7c82c505cf0ff048d2
Anya Finkelstein <anyafink@gmail.com>
2017-04-29 00:56:26 +0700

    Implemented color-change and fine-tuned grid

--- js/main.js
+++ js/main.js
@@ -1,18 +1,40 @@
 $(document).ready(function () {
+
 var num = 16;
-for(var i = 0; i < (num); i++) {
-	var $row = $('<div class="row"></div>');
-	$('.container').append($row.clone());
+
+function genGrid() {
+	
+	for(var i = 0; i < (num); i++) {
+		var $row = $('<div class="row"></div>');
+		$('.container').append($row.clone());
+	}
+
+	for(var i = 0; i < (num); i++) {
+		var $square = $('<div class="square"></div>');
+		$('.row').append($square.clone());
+	}
+
+	var calc = 100/num;
+	calc = calc - (calc * .01);
+
+	$('.row').css({'height': calc + '%'});
+	// $('.square').css({'height': (100/num) + '%'});
+	$('.square').css({'width': calc + '%'});
+
+	$('.square').on('mouseenter', function() {
+		$(this).addClass('color');
+		var currentColor = $(this).css('background-color');
+		$(this).css({'border-color': currentColor});
+	});
 }
 
-for(var i = 0; i < (num); i++) {
-	var $square = $('<div class="square"></div>');
-	$('.row').append($square.clone());
-}
+genGrid();
 
+$('button').on('click', function() {
+	$('.row').remove();
+	num = +(prompt('How many squares, yo?'));
+	genGrid();
 
-$('.row').css({'height': ((100/num) - 1) + '%'});
-// $('.square').css({'height': (100/num) + '%'});
-$('.square').css({'width': ((100/num) - 1) + '%'});
+});
 
 });

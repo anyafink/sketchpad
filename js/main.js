6541fb24c33019ad57b0b3c2f24f9982c70557c9
Anya Finkelstein <anyafink@gmail.com>
2017-04-29 16:56:42 +0700

    Final commit for index.html, main.js, and style.css

--- js/main.js
+++ js/main.js
@@ -18,23 +18,86 @@ function genGrid() {
 	calc = calc - (calc * .01);
 
 	$('.row').css({'height': calc + '%'});
-	// $('.square').css({'height': (100/num) + '%'});
 	$('.square').css({'width': calc + '%'});
 
-	$('.square').on('mouseenter', function() {
-		$(this).addClass('color');
-		var currentColor = $(this).css('background-color');
-		$(this).css({'border-color': currentColor});
-	});
+
 }
 
 genGrid();
 
-$('button').on('click', function() {
+$('#redo').on('click', function() {
 	$('.row').remove();
 	num = +(prompt('How many squares, yo?'));
 	genGrid();
+	genSolid();
+
+if(!$('#colorChange').data('toggle')) {
+	genRainbow();
+	$('#colorChange').text('Solid');
+	} else {
+		genSolid();
+		$('#colorChange').text('Rainbow');
+	}
+	$('#colorChange').data('toggle', !$('#colorChange').data('toggle'));
+
 
 });
+
+function genSolid() {
+$('.square').on('mouseenter', function() {
+	$(this).css({'background-color': 'rgba(0, 255, 204, .5)'});
+	var currentColor = $(this).css('background-color');
+	$(this).css({'border-color': currentColor});
+	});
+}
+genSolid();
+
+function genRainbow() {
+	$('.square').on('mouseenter', function() {
+	    var letters = '0123456789ABCDEF';
+	    var color = '#';
+	    for (var i = 0; i < 6; i++ ) {
+	        color += letters[Math.floor(Math.random() * 16)];
+	    }
+	$(this).css({'background-color': color});
+	$(this).css({'border-color': color});
+	});
+}
+
+function genColor() {
+	$('#colorChange').on('click', function() {
+	$('.square').css({'background-color': 'transparent'});
+	$('.square').css({'border-color': '#F2F2F2'});
+	if(!$(this).data('toggle')) {
+	genRainbow();
+	$('#colorChange').text('Solid');
+	} else {
+		genSolid();
+		$('#colorChange').text('Rainbow');
+	}
+	$(this).data('toggle', !$(this).data('toggle'));
+});
+}
+genColor();
+
+var opacity = .01;
+
+$('#greyScale').on('click', function() {
+	opacity = .01
+	$('.square').css({'background-color': 'transparent'});
+	$('.square').css({'border-color': '#F2F2F2'});
+	$('.square').on('mouseenter', function() {
+		$(this).css({'background-color': 'rgba(0, 0, 0, ' + opacity});
+		var currentColor = $(this).css('background-color');
+		$(this).css({'border-color': currentColor});
+		if (opacity < 1) {
+		opacity += .01;
+		} else {
+			opacity = .01;
+		}
+	});
+});
+
+
 
 });
